1dabe3f85565f2df6c864f8739329b9413aad277
aishwaryatalapuru <aishwaryatalapuru@gmail.com>
2017-02-02 19:36:08 +0530

    tab activity new try

---
+++ app/src/main/java/com/appstone/www/tabactivity/DemoViewHolder.java
@@ -0,0 +1,24 @@
+
+package com.appstone.www.tabactivity;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+
+
+public abstract class DemoViewHolder extends RecyclerView.ViewHolder {
+
+
+    public TextView title;
+
+
+    public DemoViewHolder(View view) {
+        super(view);
+
+
+        this.title = (TextView) view.findViewById(R.id.cardTitle);
+
+    }
+
+
+}
